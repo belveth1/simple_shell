a2c16cd43bb2bd3d8ee91d042d846e0a0003def0
mohammed hosam <mohamedhosamopp122@icloud.com>
2023-09-22 19:11:32 -0700

    me

---
+++ set.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * help_env - Displays information on the shell by builtin command 'env'
+ */
+void h_env(void)
+{
+	char *msg = "env: env\n\tPrints the current environment.\n";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
+
+/**
+ * h_setenv - Displays information on the shell by builtin command 'setenv'
+ */
+void h_isetenv(void)
+{
+	char *msg = "setenv: setenv [VARIABLE] [VALUE]\n\tInitializes a new";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "environment variable, or modifies an existing one.\n\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "\tUpon failure, prints a message to stderr.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
+
+/**
+ * h_unsetenv - Displays information on the shellby builtin command
+ * 'unsetenv'
+ */
+void h_unsetenv(void)
+{
+	char *msg = "unsetenv: unsetenv [VARIABLE]\n\tRemoves an ";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "environmental variable.\n\n\tUpon failure, prints a ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "message to stderr.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
